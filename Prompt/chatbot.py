1417693fcc5b49b77781660ffddb5a9b9c134914
RifatSarker <rifatsarkercse@gmail.com>
2025-09-01 08:49:15 +0600

    push messageplaceholder

--- Prompt/chatbot.py
+++ Prompt/chatbot.py
@@ -1,5 +1,6 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 
 load_dotenv()
 
@@ -9,10 +10,10 @@ model=ChatGroq(model="llama-3.1-8b-instant",temperature=0.7)
 history=[]
 while True:
     user_input=input("Human:")
-    history.append(user_input)
+    history.append(HumanMessage(content=user_input))
     if user_input=="exit":
         break
     result=model.invoke(history)
-    history.append(result.content)
+    history.append(AIMessage(content=result.content))
     print("AI:",result.content)
 print(history)
