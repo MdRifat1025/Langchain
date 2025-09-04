a6b884622d9d8fa31bc9f4bcf231a806e3b2095a
RifatSarker <rifatsarkercse@gmail.com>
2025-09-04 10:28:08 +0600

    pushstructuredoutput

---
+++ structureoutput/structurejson.py
@@ -0,0 +1,62 @@
+from pydantic import BaseModel, Field
+from typing import Literal,Optional
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import json
+
+load_dotenv()
+json_schema = {
+  "title": "Review",
+  "type": "object",
+  "properties": {
+    "key_themes": {
+      "type": "array",
+      "items": {
+        "type": "string"
+      },
+      "description": "Write down all the key themes discussed in the review in a list"
+    },
+    "summary": {
+      "type": "string",
+      "description": "A brief summary of the review"
+    },
+    "sentiment": {
+      "type": "string",
+      "enum": ["pos", "neg"],
+      "description": "Return sentiment of the review either negative, positive or neutral"
+    },
+    "pros": {
+      "type": ["array", "null"],
+      "items": {
+        "type": "string"
+      },
+      "description": "Write down all the pros inside a list"
+    },
+    "cons": {
+      "type": ["array", "null"],
+      "items": {
+        "type": "string"
+      },
+      "description": "Write down all the cons inside a list"
+    },
+    "name": {
+      "type": ["string", "null"],
+      "description": "Write the name of the reviewer"
+    }
+  },
+  "required": ["key_themes", "summary", "sentiment"]
+}
+
+
+
+model=ChatGroq(model="llama-3.1-8b-instant", temperature=0.7)
+
+    
+
+structured_model=model.with_structured_output(json_schema)
+
+result=structured_model.invoke("""
+Extract the name of the student from the following text. Return only valid JSON. """)
+
+
+print(result)
--- structureoutput/typedict.py
+++ structureoutput/typedict.py
@@ -1,21 +1,31 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
-from typing import TypedDict
+from typing import TypedDict,Annotated,Literal,Optional
 
 load_dotenv()
 
 class Review(TypedDict):
-    summary: str
-    sentiment: str
+    key_themes: Annotated[list[str], "Write down all the key themes discussed in the review in a list"]
+    summary: Annotated[str, "A brief summary of the review"]
+    sentiment: Annotated[Literal["pos", "neg"], "Return sentiment of the review either negative, positive or neutral"]
+    pros: Annotated[Optional[list[str]], "Write down all the pros inside a list"]
+    cons: Annotated[Optional[list[str]], "Write down all the cons inside a list"]
+    name: Annotated[Optional[str], "Write the name of the reviewer"]
 
 model = ChatGroq(model="llama-3.1-8b-instant", temperature=0.7)
 
 structured_model = model.with_structured_output(Review)
 
-result = structured_model.invoke("""
-Gowtam Tinnanuri, known for his
-         emotionally engaging storytelling in Jersey, brings a similar sensibility to Kingdom. 
-    Though mounted on a broader canvas with action and fictional history woven in, the film never loses sight of its emotional core. Kingdom taps into a classic saviour myth, where a displaced people hold on to ancestral belief that one day, someone bearing the signs of their ancient wisdom will lead them home.
+result =structured_model.invoke("""
+Analyze the following review. Extract key themes, write a brief summary, determine sentiment 
+(positive, negative, or neutral), list pros and cons, and include the reviewer name if available.
+Return only valid JSON.
+
+Review text:
+Gowtam Tinnanuri, known for his emotionally engaging storytelling in Jersey, brings a similar sensibility to Kingdom. 
+Though mounted on a broader canvas with action and fictional history woven in, the film never loses sight of its emotional core. 
+Kingdom taps into a classic saviour myth, where a displaced people hold on to ancestral belief that one day, someone bearing the signs of their ancient wisdom will lead them home.
 """)
+
 
 print(result)
